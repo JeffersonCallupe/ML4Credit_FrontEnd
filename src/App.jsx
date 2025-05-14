98e7e50e5a8d7d26b651fd78d0cdc3ab62a10d3f
Jefferson J. <jeff.callupe@gmail.com>
2025-05-14 08:09:32 -0500

    initial Dashboards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button"
+import Page from "./app/dashboard/Page"
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <Page>
+      
+    </Page>
   )
 }
 
